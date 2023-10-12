b01ff45acd30d39f73ebcee9229648d0e33d0c76
SkippyWalnuts <beccaearly89@gmail.com>
2023-10-12 15:11:42 +0100

    Created Hero, Application and External Links components

---
+++ src/ExternalLinks.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ExternalLinks() {
+    return (
+        <div className="ExternalLinks">
+        <a href="https://hbr.org/2019/10/why-reverse-mentoring-works-and-how-to-do-it-right"
+        rel="noreferrer" target="_blank" className="learn-more">
+        👉🏽Why Reverse Mentoring Works and How to Do It Right : Jennifer Jordan and Michael Sorell👈🏾
+    </a>
+    </div>
+    )
+}
---
+++ src/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero() {
+    return (
+<div className="Hero mb-5">
+        <h1>
+           Reverse <div>Mentoring</div>
+        </h1>
+        <h2>
+            Supporting the development of inclusive leadership
+        </h2>
+ 
+    
+    <img src="./images/reverse-mentoring.webp"
+    className="img-fluid rev-mentor-img"    
+    alt="Reverse Mentoring" id="person pointing to a ladder against a wall of locks, while the person next to them holds a key"/>
+    </div>
+    );
+}
