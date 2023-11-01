5bdf1c7158cba275190d63f4fd33ec3b49ea59a5
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-01 15:06:28 +0000

    Added home id and new components Contact & About

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Hero from "./Hero";
+import Contact from "./Contact";
+import About from "./About";
 import RevNavbar from "./RevNavbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="home">
 <RevNavbar />
 <Hero />
+<Contact />
+<About />
 
 
     </div>
