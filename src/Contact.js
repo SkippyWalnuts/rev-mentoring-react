a4f038c693153286abb6eddb97b8b9470f61b769
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-01 15:06:59 +0000

    Created Contact component, container and css

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Contact.css";
+
+export default function Contact() {
+    return (
+        <div className="Contact">
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="row">
+                    <div className="col contact-link">
+                        <button>Contact</button></div>
+                    <div className="col contact-link">
+                        <button>Github</button></div>
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
