1311534906a4aca1f9351b96d23e0431b61985df
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-01 18:25:27 +0000

    Updated css for responsiveness, added Brene Brown quote

--- src/About.js
+++ src/About.js
@@ -3,24 +3,26 @@ import "./About.css";
 
 export default function About() {
     return (
-    <div className="About" id="about">
+    <div className="About">
 <div className="container-fluid">
-<div className="row">
-    <div className="col about-col">
-    <img src="../images/reverse-mentoring.webp" className="img-fluid" 
-    alt="person pointing up a ladder against a wall of locks, as another person looks up holding a key"></img>
-    </div>
-    <div className="col-4 about-col">
-        <h3>About Reverse Mentoring</h3>
+    <div className="row align-items-center">
+        
+        <div className="col about-col">
+        <h3>
+            <div className="about-title" id="about">About</div>
+            Reverse Mentoring</h3>
+         </div>
+        <div className="col about-col">
+        <img src="../images/reverse-mentoring.webp" className="img-fluid" 
+        alt="person pointing up a ladder against a wall of locks, as another person looks up holding a key"></img>
         </div>
-
+    </div>
+    <div className="row">
+                <h4><em>Dr. Brené Brown:</em> "…rarely can a <strong>response</strong> make something better. 
+                     What makes something better is <strong>connection</strong>"​</h4>
+                </div> 
+</div>      
 </div>
-
-       
-       
-       
-       </div>      
-       </div>
        
        );
 }
