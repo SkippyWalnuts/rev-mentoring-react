939c1472be8b359cf7ab828773657f6119a17976
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-01 15:05:55 +0000

    Created About component, added image and css

---
+++ src/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+    return (
+    <div className="About" id="about">
+<div className="container-fluid">
+<div className="row">
+    <div className="col about-col">
+    <img src="../images/reverse-mentoring.webp" className="img-fluid" 
+    alt="person pointing up a ladder against a wall of locks, as another person looks up holding a key"></img>
+    </div>
+    <div className="col-4 about-col">
+        <h3>About Reverse Mentoring</h3>
+        </div>
+
+</div>
+
+       
+       
+       
+       </div>      
+       </div>
+       
+       );
+}
