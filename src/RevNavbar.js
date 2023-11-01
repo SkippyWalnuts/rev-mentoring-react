77bf1e5825a7e48775075435b9aa8da19c29f0af
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-01 11:20:55 +0000

    Updated Navbar css to match page theme

--- src/RevNavbar.js
+++ src/RevNavbar.js
@@ -1,21 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./RevNavbar.css";
 
 
 export default function RevNavbar() {
   return (
     <div className="RevNavbar">
-    <Navbar expand="sm">
-      <Container>
-        <Navbar.Brand className="brand" href="#home">Reverse Mentoring</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar collapseOnSelect expand="sm">
+      <Container className="nav-container">
+        <Navbar.Brand className="nav-brand" href="#home">Reverse Mentoring</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+            <Nav.Link href="#link" className="nav-link">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
