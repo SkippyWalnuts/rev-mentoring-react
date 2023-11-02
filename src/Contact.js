f26a7c2ad983475cc7283d5dd479a0cd9f3198a3
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-02 14:38:52 +0000

    Added Overview button to Contact.css , added About id to Contact row

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     return (
         <div className="Contact">
             <div className="container-fluid d-flex justify-content-center">
-                <div className="row">
+                <div className="row" id="about">
                     <div className="col contact-link">
                         <button>Contact</button></div>
                     <div className="col contact-link">
