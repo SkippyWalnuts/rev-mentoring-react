39e18379045f019ccb736ce438090401b1412e23
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-17 16:39:17 +0000

    Tidied Reviews text grammar, moved Hero h1 align right

--- src/Reviews.js
+++ src/Reviews.js
@@ -26,61 +26,47 @@ export default function Reviews() {
             alt="Brené Brown's Website"
             className="quote-link">Brené Brown</a></h4>
     </div> 
-    <p>“I thoroughly enjoyed being part of the reverse mentoring scheme at Dentsu. ​
+    <p><strong>Mentor: </strong>​“I thoroughly enjoyed being part of the Reverse Mentoring scheme at (Company).
 
-Initially when it was introduced and I learned about the objectives of the scheme, I was slightly sceptical as to whether it would be effective and what exactly I would gain from it as a mentor. ​
+Initially when it was introduced and I learned about the objectives of the scheme, I was slightly sceptical as to whether it would be effective and what exactly I would gain from it as a Mentor. ​
 
-Having been paired with a great mentee and learning a lot about him, his life, his career and vice versa, I can honestly say it was a great experience and I was able to gain insights I may never have had if it wasn’t for the reverse mentoring scheme.​
+Having been paired with a great Mentee and learning a lot about him, his life, his career and vice versa, I can honestly say it was a great experience and I was able to gain insights I may never have had if it wasn’t for the Reverse Mentoring scheme.​
 
-I really hope this can continue at Dentsu and I would definitely encourage other colleagues to join. The HR team who chooses the pairings and run the mentor/mentee sessions are fantastic and they make the sessions engaging, informative and interesting.”​
+I really hope this can continue at (Company) and I would definitely encourage other colleagues to join. The team who chooses the pairings and run the Mentor/Mentee sessions are fantastic and they make the sessions engaging, informative and interesting.”​
 
-<strong>Mentor</strong>​</p>
-<p>
-“I think the premise of the reverse mentoring programme is a very unique way of uncovering and discussing unconscious biases. Awareness is key when it comes to unconscious bias.​
+</p>
+<p><strong>Mentor: </strong>
+“I think the premise of the Reverse Mentoring programme is a very unique way of uncovering and discussing unconscious biases. Awareness is key when it comes to unconscious bias.​
 
 Often, people aren’t simply aware of issues and behaviours unless highlighted, so this was a good platform for mutual openness and discussion, and bringing certain issues to the surface. ​
 
-I enjoyed my discussions with (Mentee); they were insightful for both of us. My hope is that the senior personnel ‘mentees’ bring about change within dentsu as a result of this programme. dentsu is a very diverse organisation already, but there’s always room for improvement!”​
+I enjoyed my discussions with (Mentee); they were insightful for both of us. My hope is that the senior personnel Mentees bring about change within (Company) as a result of this programme. (Company) is a very diverse organisation already, but there’s always room for improvement!”​
 
-<strong>Mentor</strong>
+
 </p>
-<p>
-“The reverse mentoring programme has been a really worthwhile investment of my time. ​
+<p><strong>Mentee: </strong>
+“The Reverse Mentoring programme has been a really worthwhile investment of my time. ​
 
-I've gained some great insights and reflections from my mentor and also benefited from the regular briefing and coaching from the expert team running the programme.”​
+I've gained some great insights and reflections from my Mentor and also benefited from the regular briefing and coaching from the expert team running the programme.”​
 
-<strong>Mentee</strong>
+
 </p>
-<p>
-“Reverse Mentoring is an excellent scheme to dedicate time away from the day-to-day to focus on I&D topics.​
-
-As mentees, we are empowered to take responsibility for our mentees’ education to bring about a positive impact on our People and therefore, drive business transformation through open, honest conversations to grow together.”​
-
-<strong>Mentor</strong>​
+<p><strong>Mentor: </strong>​
+“Reverse Mentoring is an excellent scheme to dedicate time away from the day-to-day to focus on I&D (Inclusion & Diversity) topics.​
+As Mentors, we are empowered to take responsibility for our Mentees’ education to bring about a positive impact on our people and therefore, drive business transformation through open, honest conversations to grow together.”​
 </p>
 
-<p>
+<p><strong>Mentee: </strong>
 “It has been an eye opening experience, one that I have definitely taken a lot from. And certainly one that I have mentioned multiple times to many people in the business as an initiative that I am enjoying and getting a lot out of. And of course would recommend to any senior leader.​
-
-​
-
-My favourite workshop that we did as mentees was where we shared experiences, which may have been the first one? I feel that having more of those workshops would be interesting. Also will there be summary session, where we can all share what we learned from the first 12 months?”​
-
-<strong>Mentee</strong>
+My favourite workshop that we did as mentees was where we shared experiences, which may have been the first one? I feel that having more of those workshops would be interesting...”​
 </p>
 
-<p>“Reverse mentoring is such a great initiative and for the Global Media SL is helping develop a culture of understanding among employees who come from diverse backgrounds. ​
-
-I do think it will help drive the change needed in the right way. The launch session and subsequent check in sessions with mentors and mentees have been well structured and timely.  Using a good combination of show and tell and multimedia.​
-
-On a personal level having the opportunity to learn about areas that I’m not familiar with e.g. metaverse has been really powerful.  I also think it has helped my mentor as they have had to think about their content and communication style to ensure that I understand!​
-
-The one build I’d suggest is in the launch session, to run through a couple of different types of reverse mentoring scenarios e.g. knowledge sharing, experience sharing could help ensure greater clarity around the how and what is required from the mentor relationship.”​
-
-<strong>Mentee</strong>
-
+<p><strong>Mentee: </strong>
+“Reverse mentoring is such a great initiative and for the (Team) in helping develop a culture of understanding among employees who come from diverse backgrounds. ​
+I do think it will help drive the change needed in the right way. The launch session and subsequent check in sessions with Mentors and Mentees have been well structured and timely.  Using a good combination of show and tell and multimedia.​
+On a personal level having the opportunity to learn about areas that I’m not familiar with, e.g. Metaverse, has been really powerful.  I also think it has helped my Mentor as they have had to think about their content and communication style to ensure that I understand..!”​
 ​</p>
-    </div>
+</div>
 </div>
 
     );
