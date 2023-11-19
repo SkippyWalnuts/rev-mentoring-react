884865a5ae73c8b88f542aef805b52065257c8aa
SkippyWalnuts <beccaearly89@gmail.com>
2023-11-19 15:51:41 +0000

    Edited Hero ratings, added FontAwesome stars

--- src/Hero.js
+++ src/Hero.js
@@ -26,31 +26,51 @@ export default function Hero() {
         </div>
         <div className="row">
         <div className="col">
-        <ul>
-            <li>
-                Mentors ⭐4.8 / 5⭐
-            </li>
-            <li> Mentees ⭐4.8 / 5⭐</li>
-
-        </ul>
+        <h3>
+            How helpful has our Reverse Mentoring been at your company?
+        </h3>
         </div>
         <div className="col">
-        <ul>
-            <li>Mentors ⭐4.7 / 5⭐</li>
-            <li>Mentees ⭐5 / 5⭐</li>
-        </ul>
+        <h3>
+         Would you recommend Reverse Mentoring to a colleague?
+        </h3>
         </div>
         </div>
         <div className="row">
         <div className="col">
-        <h3>
-            How helpful Reverse Mentoring has been at their company
-        </h3>
+               <div className="ratings">  Mentors{" "}
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star-half-stroke"></i> 4.8
+                </div>
+                 <div className="ratings">
+                Mentees{" "}
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star-half-stroke"></i> 4.8
+                </div>
         </div>
         <div className="col">
-        <h3>
-         Would recommend Reverse Mentoring to a colleague
-        </h3>
+             <div className="ratings">
+                Mentors{" "}
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star-half-stroke"></i> 4.7
+                </div> 
+                 <div className="ratings">
+                Mentees{" "}
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i> 5.0
+</div>
         </div>
         </div>
         <div className="col">
