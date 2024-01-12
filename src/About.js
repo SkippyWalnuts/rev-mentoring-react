83ca29a8f41419b40e00495af8eae605ed254a33
SkippyWalnuts <beccaearly89@gmail.com>
2024-01-12 13:29:35 +0000

    Updated colour palette, title-row class & title-img properties

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ export default function About() {
     return (
     <div className="About">
 <div className="container-fluid">
-    <div className="row align-items-center">
+    <div className="row title-row align-items-center">
         
         <div className="col title-col">
         <h3>
@@ -14,7 +14,7 @@ export default function About() {
          </div>
         <div className="col title-col">
        
-        <img src="../images/about_undraw.png" className="img-fluid" 
+        <img src="../images/about_undraw.png" className="img-fluid title-img" 
         alt="a person in a wheelchair holds a phone in one hand while raising a comment box above their head"></img>
         </div>
     </div>
@@ -35,18 +35,18 @@ export default function About() {
         ​</p>
 </div>
         <div className="col">
-        <p className="p-right">​Reverse Mentoring helps your business harness the power 
+        <p>​Reverse Mentoring helps your business harness the power 
         of human <em>connection</em> in a virtual world...without the survey-fatigue!</p> 
         </div>
     </div>
     <div className="row mx-5">
-    <h3>why reverse mentoring is good for you, your employees & your business</h3>
+    <h5>Why Reverse Mentoring is good for you, your employees & your business</h5>
     <iframe src="https://streamly.video/embed/video/why-reverse-mentoring-is-good-for-you-your-employees-and-your-business" 
     scrolling="no" 
     allowFullScreen={true} 
     title="Becca Early explains why reverse mentoring is good for you, your employees and your business"
     className="streamly-video"> </iframe>
-    <h3>About Becca Early</h3>
+    <h5>About Becca Early</h5>
     <div className="about-becca">
     <p> 20 years of working in support roles taught Becca how to manage 
         busy schedules as effectively as possible, whilst finding it impossible to ignore the severe
