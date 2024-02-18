40ccf098bf9593ae3300c859b373c770cfb408c2
SkippyWalnuts <beccaearly89@gmail.com>
2024-02-18 17:24:13 +0000

    Removed about-becca section and css, updated about text

--- src/About.js
+++ src/About.js
@@ -31,25 +31,25 @@ export default function About() {
             </h4>
 
     </div> 
-    <div className="row align-items-top about-row">
+    <div className="row about-row">
         <div className="col-sm">
-        <p> A Better Understanding teaches teams how to 
+        <p> 
+
+            A Better Understanding teaches teams how to 
             <strong>{" "}stay connected across digital work-environments</strong>, 
             empowering employees to<strong>{" "}Mentor-the-Managers</strong>, 
-            and encourage the<strong>{" "}equitable inclusivity{" "}</strong> 
-            business decisions often miss out on.
-            <div className="about-bold">
-            Goodbye survey-fatigue!
+            and encourage <strong>{" "}equitably inclusive decision-making{" "}</strong>
+            in company board-rooms.
+            <div className="about-bold mb-0">
+            Goodbye, survey-fatigue!
             </div> 
         ​</p>
 </div>
-
         <div className="col">
-           
-       <p> 
+            <p> 
              A Better Understanding offers a <strong>safe space{" "}</strong> 
-             to build bridges with <strong>vulnerability and friendship{" "}</strong> 
-             with mediated, peer-led discussions about: 
+             to build bridges with <strong>vulnerability and friendship</strong>, 
+             guided by mediated, peer-led discussion topics: 
             <div className="about-bold workshops">
             <ul>
             <li>Empathy</li>
@@ -62,10 +62,16 @@ export default function About() {
         </div>
     </div>
 
-    <div>
-    <h5>Do We Have the Time (and Budget...)?</h5>
-    <p>Designed and led by a team of Assistants and Administrators, we know how to create sustainable bridges
-        between employees and their managers. 
+    <div className="row about-row">
+    <h5>Do We Have the Time <div>(and Budget...)?</div></h5>
+    <p>A Better Understanding uses <strong>The Power of The PA{" "}</strong>
+    to create <strong>sustainable</strong> bridges between employees and their managers. Designed 
+    and led by a team of Personal and Executive Assistants, <strong>scheduling efficiency</strong> is 
+    a key ingredient to success, which is often missed from other management courses. 
+       <br />
+       <br />
+        
+         
     </p>
     </div>
 
@@ -77,29 +83,8 @@ export default function About() {
     title="Becca Early explains why reverse mentoring is good for you, your employees and your business"
     className="streamly-video"> </iframe>
     <h5>About Becca Early</h5>
-    <div className="about-becca">
-    <p> 20 years of working in support roles taught Becca how to manage 
-        busy schedules as effectively as possible, whilst finding it impossible to ignore the severe
-        lack of attendee diversity within crucial business meetings. 
-        <br />
-        <br />
-        Following a year of mentoring with dentsu UK's Reverse Mentoring pilot, Becca was asked to project-manage the 
-        programme for dentsu Global's Media service-line. Drawing upon an understanding of how to capture the attention 
-        of busy Senior Leaders, with an aim to restore employee faith in equitable leadership, Becca built a framework 
-        of workshops, peer-led group discussions and schedule-simplicity. 
-        <br />
-        <br />
-        Global Media's reviews and 5-star ratings caught the interest of many others at dentsu. In fact, participant 
-        numbers doubled by Becca's second year of project-management, with multiple service lines of varying international locations 
-        requesting access to apply. Many needed to be wait-listed for future rollouts, demonstrating just how important Reverse Mentoring 
-        had become to the organisation on a truly global scale. 
-        <br />
-        <br />
-        Becca's Reverse Mentoring has the potential to restore human connection within businesses and industries 
-        in an increasingly virtual world. For support in developing inclusive leadership within your organisation, 
-        arrange your free consultation call today!
-        </p>
-    </div>
+    
+    
     </div>
 </div>   
 <span id="reviews"></span>   
