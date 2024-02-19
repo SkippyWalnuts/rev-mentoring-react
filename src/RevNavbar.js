2f3b6b2a9a206e15cb8cc30a28cef6509b0c3844
SkippyWalnuts <beccaearly89@gmail.com>
2024-02-19 09:47:48 +0000

    Changed Homepage links to refresh page

--- src/RevNavbar.js
+++ src/RevNavbar.js
@@ -9,11 +9,11 @@ export default function RevNavbar() {
     <div className="RevNavbar">
     <Navbar collapseOnSelect expand="md" fixed="top" className="nav-navbar">
       <Container className="nav-container">
-        <Navbar.Brand className="nav-brand nav-company-name nav-text" href="/#home">A Better Understanding</Navbar.Brand>
+        <Navbar.Brand className="nav-brand nav-company-name nav-text" href="/">A Better Understanding</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/#home" title="Homepage" className="nav-title nav-page nav-text">Home</Nav.Link>
+            <Nav.Link href="/" title="Homepage" className="nav-title nav-page nav-text">Home</Nav.Link>
             <Nav.Link href="/#about" title="About Reverse Mentoring" className="nav-link nav-page nav-text">About</Nav.Link>
             <Nav.Link href="/#reviews" title="Reverse Mentoring Reviews" className="nav-link nav-page nav-text">Reviews</Nav.Link>
             <Nav.Link href="mailto:becca.early@outlook.com?subject=A Better Understanding Query" title="Email Becca Early with A Better Understanding queries" className="nav-link nav-page nav-text">Contact</Nav.Link>
