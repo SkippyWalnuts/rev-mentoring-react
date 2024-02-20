9c35c7d1eb224e810cde1e11489f94213d8083c1
SkippyWalnuts <beccaearly89@gmail.com>
2024-02-20 12:30:38 +0000

    Added footer links and styling

--- src/App.js
+++ src/App.js
@@ -14,11 +14,16 @@ function App() {
 <Reviews defaultSection="Mentor Reviews" />
 <div class="container container-fluid footer-container fixed-bottom">
 <div class="d-flex justify-content-center">
-  <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Ftherealtops" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
-  <a href="https://www.facebook.com/ukTOpS/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-<p>Copyright A Better Understanding 2022. All rights reserved</p>
-<a href="https://www.instagram.com/uktopsofficial/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-<a href="https://www.youtube.com/channel/UC9eoFzJrTXfVLf5PLiTQ_yg" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+  <a href="https://www.linkedin.com/in/becca-early/" 
+  target="_blank"
+  rel="noreferrer"
+  alt="Becca Early, Reverse Mentor Consultant, LinkedIn Profile">
+    <i class="fa-brands fa-linkedin"></i></a>
+<p>A Better Understanding 2022</p>
+<a href="https://www.instagram.com/a_becca_understanding" 
+target="_blank"
+rel="noreferrer"
+alt="Becca Early AKA a-becca-understanding, Reverse Mentor Consultant, Instagram Profile"><i class="fa-brands fa-instagram"></i></a>
 </div>
 </div>
     </div>
